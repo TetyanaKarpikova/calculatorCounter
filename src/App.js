ef7683e09338707654d92f929f307f303394f24a
Tetyana <tetyanajkn@gmail.com>
2021-01-09 15:53:09 +0200

    initial comment

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { createRef } from 'react';
+import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+
+function App() {
+  const counter = useSelector(({ counter }) => counter);
+  const dispatch = useDispatch();
+  const inputNum = createRef();
+
+  const clickIncOne = () => { dispatch({ type: 'INC_COUNTER_ONE' }) }
+  const clickDecOne = () => { dispatch({ type: 'DEC_COUNTER_ONE' }) }
+  const clickIncHundred = () => { dispatch({ type: 'INC_COUNTER_HUNDRED' }) }
+  const clickDecHundred = () => { dispatch({ type: 'DEC_COUNTER_HUNDRED' }) }
+  const clickReset = () => { dispatch({ type: 'RESET_COUNTER' }) }
+  const clickSubmit = () => {
+    const res = inputNum.current.value.replace(/[^0-9]/g, '');
+    if (res) {      
+      dispatch({ type: 'SUBMIT_COUNTER', payload: +res });  
+    }
+    return inputNum.current.value = null;
+  }
+
+  return (
+    <div className="App">
+
+      <section>
+        <h1>Calculator</h1>
+        <p>{counter}</p>
+        <div className='flex-content margin10'>
+          <button className='w100' onClick={clickIncOne}>1</button>
+          <button className='w100' onClick={clickDecOne}>- 1</button>
+        </div>
+        <div className='flex-content margin10'>
+          <button className='w100' onClick={clickIncHundred}>100</button>
+          <button className='w100' onClick={clickDecHundred}>- 100</button>
+        </div>
+        <div>
+          <button className='w100 margin10' onClick={clickReset}>Reset</button>
+        </div>
+        <label>Число:</label>
+        <input type='text' ref={inputNum}></input>
+        <button onClick={clickSubmit}>Submit</button>
+
+      </section>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,30 @@
+const initialState = {
+    counter: 0
+}
+
+export const reducer = (state=initialState, action) => {
+    switch (action.type) {
+        case 'INC_COUNTER_ONE': {
+            return {...state, counter: state.counter + 1};
+        }
+        case 'DEC_COUNTER_ONE': {
+            return {...state, counter: state.counter - 1};            
+        }
+        case 'INC_COUNTER_HUNDRED': {
+            return {...state, counter: state.counter + 100};            
+        }
+        case 'DEC_COUNTER_HUNDRED': {
+            return {...state, counter: state.counter - 100};
+        }
+        case 'RESET_COUNTER': {                     
+            return {...state, counter: 0};
+        }
+        case 'SUBMIT_COUNTER': {              
+            return {...state, counter: state.counter + action.payload};
+        }
+
+        default: {
+            return state;
+        }
+    }
+}
